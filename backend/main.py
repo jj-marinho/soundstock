8063b6148e50c9171a693fdfe6427b087862f860
CaioBiruleibe-at-399476121720 <caio.joanoni@gmail.com>
2022-07-03 18:02:52 -0300

    frontend done

--- backend/main.py
+++ backend/main.py
@@ -1,101 +1,167 @@
-from flask import Flask, request
-
-from flask import Flask
-from flask import render_template
-from flask import redirect
-
-import atexit
-
+from sgbd import insercao_faixa, busca_faixa, busca_contrato
+from flask import Flask, request, render_template, redirect
 import psycopg2
+import atexit
+from decouple import config
 
-# conecta na base
 try:
-    # cria a conexao
-    print('Connecting to the PostgreSQL database...')
-    conn = psycopg2.connect(host='localhost', database='postgres',user='postgres', password='mysecretpassword')
+    # Conecta ao Banco de Dados
+    print("Conectando com PostgreSQL...")
+    conn = psycopg2.connect(host = config("HOST"), database = config("DATABASE"), user = config("USER"), password = config("PASSWORD"))
     
-    
-    # busca a versao do postgres para imprimir
-    print('PostgreSQL database version:')
+    # Imprime a Versão do Banco de Dados
     cur = conn.cursor()
-    cur.execute('SELECT version()')
+    cur.execute("SELECT version()")
+    print(f"Versão: {cur.fetchone()}")
 
-    # imprime a versao do postgres
-    db_version = cur.fetchone()
-    print(db_version)
-    
 except (Exception, psycopg2.DatabaseError) as error:
+    conn = None
     print(error)
 
-def closeConnection():
-    # fecha a conexao com o banco
+def fecha_conexao():
     if conn is not None:
         conn.close()
-        print('Database connection closed.')
+        print("Conexão com PostgreSQL fechada")
 
-# cria um app flask
+
 app = Flask(__name__)
 
+@app.route("/", methods=['GET'])
+def home():    
+    # cria um cursor e executa o sql
+    cur = conn.cursor()
+    print(busca_faixa())
+    cur.execute(busca_faixa())
+    recset = cur.fetchall()
+    returnData = ""
+    # faz um loop entre todos os resultados e vai juntando em itens de uma lista para o html
+    for rec in recset:
+        returnData += f'<li class="list-group-item">Nome: {rec[1]}<br>Preço: R${rec[2]}<br>Avaliação: {rec[3]}<br>Duração: {rec[5]}</li>'
+
+    return render_template('consulta_output.html', message=returnData)
+
+
 # mostra a pagina de inserir dados 
-@app.route("/")
-def index():
-    return render_template('insercao_input.html')
+@app.route("/nova-faixa")
+def novaFaixa():    
+    cur = conn.cursor()
+    cur.execute(busca_contrato())
+    recset = cur.fetchall()
+    returnData = ""
+    # faz um loop entre todos os resultados e vai juntando em itens de uma lista para o html
+    contratos = ""
+    for rec in recset:
+        print(rec)
+        contratos += f"""<div class="mb-3 d-flex  flex-row justify-content-around">
+                        <div class="form-check" style="width:300px;">
+                        <input class="form-check-input" type="checkbox" value="{rec[1]}" id="check{rec[1]}" name="check{rec[1]}">
+                        <label class="form-check-label" for="defaultCheck{rec[1]}">
+                            Contrato com o {rec[0]} com a descrição {rec[3]}
+                        </label>
+                        </div>
+                    </div>"""
+    return render_template('insercao_input.html', contratos=contratos)
 
 # recebe os dados da pagina de insercao e insere na base
-@app.route("/denuncia", methods=['POST'])
-def newFireReport():
+@app.route("/faixa-audio", methods=['POST'])
+def novaFaixaPost():
     # busca os dados da requisicao e cria o sql para insercao, aqui seria necessario evitar sql injection
-    sql = """INSERT INTO public.denuncia(timestamp_den, descricao_endereco, descricao_ocorrencia, cpf, modo_acesso, ip, n_contato) 
-    VALUES (current_timestamp, '{address}', '{description}', '{cpf}', 'INTERNET', '0.0.0.0', NULL);""".format(address=request.form['address'], 
-    description=request.form['description'], cpf=request.form['cpf'])
+    print(request.form.to_dict())
+    form = request.form.to_dict()
+    contratos = []
+    for key in form:
+        print(key, form[key])
+        if "check" in key:
+            print("checkbox", key)
+            contratos.append(form[key])
+    idiomas = []
+    for idioma in form['idiomas'].split(","):
+        if idioma != "":
+            idiomas.append(idioma.strip())
+    generos = []
+    for genero in form['generos'].split(","):
+        if genero != "":
+            generos.append(genero.strip())
+    instrumentos = []
+    for instrumento in form['instrumentos'].split(","):
+        if instrumento != "":
+            instrumentos.append(instrumento.strip())
+    nome = form['nome']
+    preco = float(form['preco'])
+    duracao = form['duracao'] + " seconds"
+    
+
+    print( insercao_faixa(nome, preco, duracao, contratos, generos, idiomas, instrumentos))
+    cur = conn.cursor()
     # executa o sql
-    cur.execute(sql)
+    cur.execute( insercao_faixa(nome, preco, duracao, contratos, generos, idiomas, instrumentos))
     # comita a operacao
     conn.commit()
+   
+    # sql = """BEGIN
+    #         INSERT INTO Produto VALUES (...) RETURNING codigo into val_codigo;""".format(address=request.form['address'], 
+    #             description=request.form['description'], cpf=request.form['cpf'])
+
+
+    # sql = sql + "COMMIT"
+            
+    # # executa o sql
+    # cur.execute(sql)
+    # # comita a operacao
+    # conn.commit()
+
     # redireciona para a pagina onde mostra que a insercao foi bem sucedida
-    return redirect("/denuncia", code=302)
+    return redirect("/", code=302)
 
 # mostra todas as insercoes feitas na base
-@app.route("/denuncia", methods=['GET'])
-def getFireReport(): 
-    # sql com todas as insercoes
-    sql = 'SELECT timestamp_den, descricao_endereco, descricao_ocorrencia, cpf, modo_acesso, ip, n_contato FROM public.denuncia;'
-    # cria um cursor e executa o sql
-    cur = conn.cursor()
-    cur.execute(sql)
-    recset = cur.fetchall()
-    returnData = ""
-    # faz um loop entre todos os resultados e vai juntando em itens de uma lista para o html
-    for rec in recset:
-        returnData += '<li class="list-group-item">{row}</li>'.format(row=rec)
-    # retorna para o usuario a pagina
-    return render_template('insercao_output.html', message=returnData)
+@app.route("/busca", methods=['GET'])
+def formBuscarFaixa(): 
 
-# mostra a consulta escolhida pelo grupo
-@app.route("/consulta", methods=['GET'])
-def getSelect(): 
-    # sql escolhido
-    sql = """select C.nome, extract (year from Q.timestamp_finalizacao) as "ano", count(distinct Q.id) as "quantidade"
-                from corpo_bombeiros C join veiculos V on C.cnpj = V.corpo_bombeiros
-                       join despacho_veiculo D on V.placa = D.veiculo
-					   join queimada Q on D.queimada = Q.id
-					   where upper(Q.status) = 'FINALIZADO' 
-					   group by C.cnpj, extract (year from Q.timestamp_finalizacao);"""
+    return render_template('consulta_input.html')
+
+@app.route("/busca-resultado", methods=['POST'])
+def buscarFaixa():
+    print(request.form.to_dict())
+    form = request.form.to_dict()
+
+
+    nome = form['nome']
+    precoMin = form['preco-min']
+    if precoMin == "":
+        precoMin = 0
+    else:
+        precoMin = float(precoMin)
+    precoMax = form['preco-max']
+    if precoMax == "":
+        precoMax = 99999999.99
+    else:
+        precoMax = float(precoMax)
+    duracaoMin = form['duracao-min']
+    if duracaoMin == "":
+        duracaoMin = "0 secs"
+    else:
+        duracaoMin += " secs"
+    duracaoMax = form['duracao-max']
+    if duracaoMax == "":
+        duracaoMax = "178000000 years"
+    else:
+        duracaoMax += " secs"
+
+    
     # cria um cursor e executa o sql
     cur = conn.cursor()
-    cur.execute(sql)
+    print(busca_faixa(nome=nome, duracao=(duracaoMin, duracaoMax), preco=(precoMin, precoMax)))
+    cur.execute(busca_faixa(nome=nome, duracao=(duracaoMin, duracaoMax), preco=(precoMin, precoMax)))
     recset = cur.fetchall()
     returnData = ""
     # faz um loop entre todos os resultados e vai juntando em itens de uma lista para o html
     for rec in recset:
-        returnData += '<li class="list-group-item">{row}</li>'.format(row=rec)
-    # retorna para o usuario a pagina
-    return render_template('consulta.html', message=returnData)
+        returnData += f'<li class="list-group-item">Nome: {rec[1]}<br>Preço: R${rec[2]}<br>Avaliação: {rec[3]}<br>Duração: {rec[5]}</li>'
+
+    return render_template('consulta_output.html', message=returnData)
     
 
 
-if __name__ == "__main__":
-    # roda a aplicacao  
-    app.run(debug=True)
-    # quando acabar, fecha a conexao
-    atexit.register(closeConnection)
+if __name__ == "__main__":  
+    app.run(debug = True)
+    atexit.register(fecha_conexao)
---
+++ backend/sgbd.py
@@ -0,0 +1,40 @@
+# O ID do Produtor foi fixado como sendo 3 nas queries para simular o fato de estar logado como tal Produtor.
+
+
+def insercao_faixa(nome, preco, duracao, contratos, generos, idiomas, instrumentos):
+    comando =  f"DO $$ DECLARE                                                                                                                  \n"
+    comando += f"    faixa INTEGER;                                                                                                             \n"
+    comando += f"BEGIN                                                                                                                          \n"
+    comando += f"    INSERT INTO Produto VALUES (DEFAULT, DEFAULT, '{nome}', 'Faixa de Audio', {preco :.2f}, NULL) RETURNING codigo INTO faixa; \n"
+    comando += f"    INSERT INTO Faixa_Audio VALUES (faixa, '{duracao}');                                                                       \n"
+
+    for contrato in contratos:       comando += f"    INSERT INTO Faixa_Contrato VALUES (faixa, {contrato});         \n"
+    for genero in generos:           comando += f"    INSERT INTO Genero_Faixa VALUES (faixa, '{genero}');           \n"
+    for idioma in idiomas:           comando += f"    INSERT INTO Idioma_Faixa VALUES (faixa, '{idioma}');           \n"
+    for instrumento in instrumentos: comando += f"    INSERT INTO Instrumento_Faixa VALUES (faixa, '{instrumento}'); \n"
+
+    return comando + "END $$"
+
+
+def busca_faixa(nome = "", preco = (0, 99999999.99), duracao = ("0 secs", "178000000 years")):
+    return f"""
+        SELECT DISTINCT P.codigo, P.nome, P.preco, P.avaliacao, P.inclusao, F.duracao
+            FROM Contrato C
+            JOIN Faixa_Contrato FC ON C.id = FC.contrato
+            JOIN Faixa_Audio    F  ON FC.faixa = F.produto
+            JOIN Produto        P  ON F.produto = P.codigo
+        WHERE C.produtor = 3
+            AND P.nome ILIKE '%{nome}%'
+            AND P.preco BETWEEN {preco[0] :.2f} AND {preco[1] :.2f}
+            AND F.duracao BETWEEN INTERVAL '{duracao[0]}' AND INTERVAL '{duracao[1]}'
+        ORDER BY P.inclusao DESC;
+    """
+
+
+def busca_contrato():
+    return """
+        SELECT U.nome, C.id, C.valor, C.descricao
+            FROM Contrato C
+            JOIN Usuario U ON C.prestador = U.id
+        WHERE C.produtor = 3 AND C."data" + C.duracao > NOW();
+    """
